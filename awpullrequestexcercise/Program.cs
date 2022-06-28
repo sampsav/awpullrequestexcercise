039330478fa3c963a5293650db7d509391c61872
Sampsa Vaurio <sampsa.vaurio@gmail.com>
2022-06-28 11:18:25 +0300

    first commit

---
+++ awpullrequestexcercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace awpullrequestexcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testi!");
+        }
+    }
+}
